68a145fd8a51024f772f5c18df39faee34a4a9c6
Rishikesh K V <rishikeshkaroth@gmail.com>
2024-09-24 23:31:29 +0530

    chore: Update firebase configuration to use environment variables

--- service/firebaseConfig.jsx
+++ service/firebaseConfig.jsx
@@ -7,13 +7,13 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDAXGfHdcEzcGXNEhVfdWS6_XWkEhqjgK0",
-  authDomain: "travel-buddy-8b2f4.firebaseapp.com",
-  projectId: "travel-buddy-8b2f4",
-  storageBucket: "travel-buddy-8b2f4.appspot.com",
-  messagingSenderId: "576700719578",
-  appId: "1:576700719578:web:2f96c13e6fb3a8d2afa0e3",
-  measurementId: "G-BR8D5PBZKW"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
