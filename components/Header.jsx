fe457b7fd49ae0b50beb0426de86cb862536ab14
Rishikesh K V <rishikeshkaroth@gmail.com>
2024-09-25 13:15:26 +0530

    chore: Refactor Header component and add user profile functionality

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,12 +1,39 @@
+"use client";
+import { useEffect, useState } from "react";
 import { Button } from "../components/ui/button";
 import Image from "next/image";
 import React from "react";
 
 const Header = () => {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      const userData = localStorage.getItem("user");
+      console.log(userData);
+      if (userData) {
+        setUser(JSON.parse(userData));
+      }
+    }
+  }, []);
+
   return (
     <div className="flex justify-between items-center p-5 border shadow-md">
       <Image src="/logo.svg" alt="logo" width={150} height={150} />
-      <Button>Sign In</Button>
+      {user ? (
+        <div className="flex">
+          <Button variant="outline" className="rounded-full">
+            My Trips
+          </Button>
+          <img
+            src={user?.picture}
+            alt={user?.name}
+            className="rounded-full size-10 ml-2"
+          />
+        </div>
+      ) : (
+        <Button href="/login">Login</Button>
+      )}
     </div>
   );
 };
